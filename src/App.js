7570d1c5e89d7bdbd40334ce8ca125b6f40f7e8f
David Taylor <dktaylor1701@gmail.com>
2017-08-12 15:16:21 -0400

    broke out register into its own component

--- src/App.js
+++ src/App.js
@@ -2,36 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Fetcher from './Fetcher';
+import Register from './Components/Register';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {}
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log(JSON.stringify({user: {username: this.state.username, email: this.state.email, password: this.state.password}}));
-    Fetcher.Auth.register(this.state.username, this.state.email, this.state.password).then(function(response){
-      console.log(response);
-      if(!response.errors){
-        Fetcher.setToken(response.user.token);
-      }
-    });
-  }
-
   render() {
     return (
       <div className="App">
@@ -39,38 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Email:
-            <input 
-              name="email"
-              type="text" 
-              value={this.state.email} 
-              onChange={this.handleChange}/>
-          </label>
-          <label>
-            Username:
-            <input 
-              name="username"
-              type="text" 
-              value={this.state.username} 
-              onChange={this.handleChange}/>
-          </label>
-          <label>
-            Password:
-            <input
-              name="password"
-              type="text"
-              value={this.state.password}
-              onChange={this.handleChange}/>
-          </label>
-          <input 
-            type="submit"
-            value="Submit" />
-        </form>
-        <div>
-          Hello {this.state.username}!
-        </div>
+        <Register />
       </div>
     );
   }
---
+++ src/Components/Register.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+import Fetcher from '../Fetcher';
+
+class Register extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify({user: {username: this.state.username, email: this.state.email, password: this.state.password}}));
+    Fetcher.Auth.register(this.state.username, this.state.email, this.state.password).then(function(response){
+      console.log(response);
+      if(!response.errors){
+        Fetcher.setToken(response.user.token);
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="Register">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input 
+              name="email"
+              type="text" 
+              value={this.state.email} 
+              onChange={this.handleChange}/>
+          </label>
+          <label>
+            Username:
+            <input 
+              name="username"
+              type="text" 
+              value={this.state.username} 
+              onChange={this.handleChange}/>
+          </label>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="text"
+              value={this.state.password}
+              onChange={this.handleChange}/>
+          </label>
+          <input 
+            type="submit"
+            value="Submit" />
+        </form>
+        <div>
+          Hello {this.state.username}!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
