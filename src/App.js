64cde32f599baa7d7036b6e6db3fa670affbad00
Gregory Hanson <astronautg117@gmail.com>
2017-08-12 13:49:38 -0400

    Added register form to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submit");
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +30,38 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input 
+              name="email"
+              type="text" 
+              value={this.state.email} 
+              onChange={this.handleChange}/>
+          </label>
+          <label>
+            Username:
+            <input 
+              name="username"
+              type="text" 
+              value={this.state.username} 
+              onChange={this.handleChange}/>
+          </label>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="text"
+              value={this.state.password}
+              onChange={this.handleChange}/>
+          </label>
+          <input 
+            type="submit"
+            value="Submit" />
+        </form>
+        <div>
+          Hello {this.state.username}!
+        </div>
       </div>
     );
   }
