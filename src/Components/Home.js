ab8817968708efd631b1f11802788ebe58789a4c
David Taylor <dktaylor1701@gmail.com>
2017-08-16 19:56:46 -0400

    removed wishes stuff

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,9 @@ class Home extends Component {
         <div className="home">
           <h2>Wishes</h2>
           <ul>
-              {this.props.wishes.map(function(wish){
+              {/* {this.props.wishes.map(function(wish){
                 return <li>{wish}</li>;
-              })}
+              })} */}
           </ul>
         </div>
     );
