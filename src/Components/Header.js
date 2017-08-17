d82580fdfa458807d82e0145849600fe5c4281df
David Taylor <dktaylor1701@gmail.com>
2017-08-16 20:05:20 -0400

    committing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,7 @@ class Header extends Component {
           <h2>Dillist</h2>
           <nav>
             <ul>
+              <li><Link to='/home'>Home</Link></li>
               <li><Link to='/login'>Login</Link></li>
               <li><Link to='/register'>Register</Link></li>
             </ul>
