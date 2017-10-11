328df42934f9e01779eb1dfe72664fc02cc6356e
David Taylor <dktaylor1701@gmail.com>
2017-10-11 19:04:04 -0400

    Added test stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+describe('App', () => {
+  beforeAll(() => {
+    const ls = require("./Utilities/localStorage.js");
+    ls.setLocalStorage();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render((
+      <Provider store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </Provider>
+    ), div);
+  });
 });
+
+
+
+
