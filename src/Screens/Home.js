33d5c21c30f9e0a98cd15c67694f36341db9b64c
Gregory Hanson <astronautg117@gmail.com>
2018-01-10 18:58:23 -0500

    Added Home.css

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,31 +1,28 @@
 import React, { Component } from "react";
-import { connect } from "react-redux"
+import { connect } from "react-redux";
+import "./Home.css";
 
 const mapStateToProps = state => ({
     currentUser: state.common.currentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-
-});
+const description = 
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+ Nullam quis semper dolor. Nunc purus nisi, elementum id.\
+ Distinxit media praeter vultus pro ligavit:\
+ persidaque flamma addidit innabilis dissaepser.\
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
 class Home extends Component {
-    constructor(props) {
-        super (props);
-    }
-
     render() {
         return (
             <div className="home">
                 <h2>Home</h2>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis semper
-dolor. Nunc purus nisi, elementum id. Distinxit media praeter vultus pro ligavit:
-persidaque flamma addidit innabilis dissaepser. Lorem ipsum dolor sit amet,
-consectetur adipiscing elit.</div>
+                <div className="description">{description}</div>
                 <button>Sign Up!</button>
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps)(Home);
